b5e497a38eb54e7a70cd40298f7b8d9d245e0e8b
Brian Kessler <brian.m.kessler@gmail.com>
2015-05-25 22:28:13 -0700

    Cleaned up some things and added asteroid generation

--- main.lua
+++ main.lua
@@ -4,53 +4,81 @@ function love.load(arg)
     height = 600
     width = 800
     love.window.setMode(width,height)
---love.physics.setMeter(64)
-    world = nil
-    player = { x = 200, y = 500, speed = 10, img = nil }
 -- Timers
 -- We declare these here so we don't have to edit them multiple places
     canShoot = true
     canShootTimerMax = 0.2
     canShootTimer = canShootTimerMax
-    text = math.sin(.2)
-    angXText = math.cos(.25)
-    angYText = math.sin(.25)
+
+   newAsteroid = true
+   newAsteroidTimerMax = 1.0
+   newAsteroidTimer = newAsteroidTimerMax
 -- Image Storage
-    bulletImg = nil
-    bulletVel = 500
-    bulletVelX = 0
-    bulletVelY = 0
-    theGround = {}
 -- Entity Storage
-    bullets = {} -- array of current bullets being drawn and updated
 
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 0, true)
 
+    player = { x = 200, y = 500, speed = 10, img = nil, r = 10 }
     player.img = love.graphics.newImage('assets/ship.png')
     player.rotation = 0
-    --we now have an asset ready to be used inside Love
-    --bulletImg = love.graphics.newImage('assets/plane.png')
-    bulletImg =  love.graphics.circle("fill", 0, 0, 10)
     player.body = love.physics.newBody(world, player.x, player.y, "dynamic")
-    player.shape = love.physics.newCircleShape(10)  -- the ship's physics is the same size as the bullets
+    player.shape = love.physics.newCircleShape(player.r)  -- the ship's physics uses circle of radius r
     player.fixture = love.physics.newFixture(player.body, player.shape) 
+    player.fixture:setUserData("player")
 
-    love.graphics.setColor(255,255,255)
+    theGround = {}
     theGround.img = love.graphics.rectangle("fill",0,0,1000,100)
-    theGround.drawn = false
     theGround.body = love.physics.newBody(world, 0, 50) --remember, the shape (the rectangle we create next) anchors to the body from its center, so we have to move it to (650/2, 650-50/2)
     theGround.shape = love.physics.newRectangleShape(1600, 50) --make a rectangle with a width of 650 and a height of 50
     theGround.fixture = love.physics.newFixture(theGround.body, theGround.shape) --attach shape to body
 
-    --theGround.body =
+    bullets = {} -- array of current bullets being drawn and updated
+    bulletImg =  love.graphics.circle("fill", 0, 0, 10) -- not being used
+    bulletVel = 500
 
-
+    assteroids ={}
 end
 
 function love.update(dt)
     world:update(dt)
---catch rotation first
+    steerShip()
+    applyThrust()
+    shootBullets(dt)
+    generateAsteroid(dt)
+    wrapScreen()
+end
+
+function love.draw()
+
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.draw(player.img, player.body:getX(), player.body:getY(), player.rotation, 1, 1, player.img:getWidth()/2, player.img:getHeight()/2)
+    love.graphics.print(text, 0, 0 )
+    love.graphics.setColor(72, 160, 14)
+    love.graphics.polygon("fill", theGround.body:getWorldPoints(theGround.shape:getPoints()))
+    for i, bullet in ipairs(bullets) do
+        if i % 2 == 1 then
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.circle("fill", bullet.body:getX(), bullet.body:getY(), bullet.shape:getRadius())
+        else
+            love.graphics.setColor(255, 0, 0)
+            love.graphics.circle("fill", bullet.body:getX(), bullet.body:getY(), bullet.shape:getRadius())
+        end
+
+    end
+    for i, assteroid in ipairs(assteroids) do
+      love.graphics.setColor(255,0,255)
+      love.graphics.circle("fill",assteroid.body:getX(), assteroid.body:getY(), assteroid.shape:getRadius())
+    end
+end
+
+function love.keypressed(key)
+   if key == 'escape' then
+      love.event.push('quit')
+   end
+end
+
+function steerShip()
     if love.keyboard.isDown('right') then --rotate right
         player.rotation = player.rotation + (.01 * math.pi)
         if player.rotation > math.pi then
@@ -66,31 +94,31 @@ function love.update(dt)
             player.rotation = player.rotation * -1
         end
     end
--- handle thrust 
+    text = player.rotation
+end
+
+function applyThrust()
     if love.keyboard.isDown('up') then -- accelerate
        player.body:applyForce(math.sin(player.rotation) * player.speed, -1*math.cos(player.rotation) * player.speed)
     end
+end
 
-    text = player.rotation
-
+function shootBullets(dt)
     if love.keyboard.isDown(' ', 'rctrl', 'lctrl', 'ctrl') and canShoot then
         -- Create some bullets
-        bullet = { x = player.body:getX() , y = player.body:getY() , img = bulletImg }
-       --bullet.hasBody = false
-        --translate players rotation into bullet dx/dy
-        bullet.dx = math.sin(player.rotation) * bulletVel
-        bullet.dy = math.cos(player.rotation) * bulletVel * -1
-        pdx, pdy = player.body:getLinearVelocity( )
-        bullet.body = love.physics.newBody(world, bullet.x+math.sin(player.rotation)*dt, bullet.y-math.cos(player.rotation)*dt, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
-        bullet.shape = love.physics.newCircleShape(5) --the ball's shape has a radius of 20
+        bullet = { x = player.body:getX() , y = player.body:getY() , img = bulletImg, r = 5 }
+        bullet.body = love.physics.newBody(world, bullet.x+math.sin(player.rotation)*(bullet.r+player.r), bullet.y-math.cos(player.rotation)*(bullet.r+player.r), "dynamic") --place the bullet at the nose ofthe ship
+        bullet.shape = love.physics.newCircleShape(bullet.r) --the ball's shape has a radius of 5
         bullet.fixture = love.physics.newFixture(bullet.body, bullet.shape, 1) -- Attach fixture to body and give it a density of 1.
         bullet.body:setBullet(true)  -- make sure it won't go through things
         bullet.fixture:setRestitution(0.9) --let the ball bounce
-
+        bullet.fixture:setUserData("bullet")  -- for use in collision ballbacks to identify bullets
+        --translate players rotation into bullet dx/dy
+        bullet.dx = math.sin(player.rotation) * bulletVel
+        bullet.dy = math.cos(player.rotation) * bulletVel * -1
         bullet.body:setLinearVelocity(bullet.dx, bullet.dy)
-
         table.insert(bullets, bullet)
-        canShoot = false
+        canShoot = false -- reset the can shoot timer
         canShootTimer = canShootTimerMax
     end
     -- Time out how far apart our shots can be.
@@ -99,40 +127,33 @@ function love.update(dt)
     else
       canShoot = true
     end
-  wrapScreen()
 end
 
-function love.draw()
-
-    love.graphics.setColor(255, 255, 255)
-    --love.graphics.print(angXText, 100, 0)
-    --love.graphics.print(angYText, 300, 0)
-    love.graphics.draw(player.img, player.body:getX(), player.body:getY(), player.rotation, 1, 1, player.img:getWidth()/2, player.img:getHeight()/2)
-    love.graphics.print(text, 0, 0 )
-    love.graphics.setColor(72, 160, 14)
-    love.graphics.polygon("fill", theGround.body:getWorldPoints(theGround.shape:getPoints()))
-    --love.graphics.draw(theGround.img, 100, 100)
-    --if theGround.drawn == false then
-        --theGround.img = love.graphics.rectangle("fill",100,100,100,100)
-        --theGround.drawn = true
-    --end
-    for i, bullet in ipairs(bullets) do
-        if i % 2 == 1 then
-            love.graphics.setColor(255, 255, 255)
-            love.graphics.circle("fill", bullet.body:getX(), bullet.body:getY(), bullet.shape:getRadius())
-        else
-            love.graphics.setColor(255, 0, 0)
-            love.graphics.circle("fill", bullet.body:getX(), bullet.body:getY(), bullet.shape:getRadius())
-        end
-
+function generateAsteroid(dt)
+   if newAsteroidTimer > 0 then
+      newAsteroidTimer = newAsteroidTimer - (1 * dt)
+    else
+      newAsteroidTimer = newAsteroidTimerMax
+      -- generate a new asteroid
+      text = 'ASSTEROID'
+      assteroid = {}
+      speed = 150
+      assteroid.radius = 64  -- assteroid size
+      math.randomseed( os.time() )
+      dir = math.random(360)
+      assteroid.dx = -1*speed*math.abs(math.sin(dir))
+      assteroid.dy = speed*math.cos(dir)
+      assteroid.x = width+assteroid.radius  -- always appear off the right edge
+      assteroid.y = math.random(height)
+      assteroid.body = love.physics.newBody(world, assteroid.x, assteroid.y, "dynamic")
+      assteroid.shape = love.physics.newCircleShape(assteroid.radius)
+      assteroid.fixture = love.physics.newFixture(assteroid.body, assteroid.shape)
+      assteroid.body:setBullet(true)  -- make sure it won't go through the paddle
+      assteroid.fixture:setRestitution(1.0)  -- make it infinitely bouncy
+      assteroid.body:setLinearVelocity(assteroid.dx, assteroid.dy)
+      assteroid.fixture:setUserData("assteroid")  -- for handling collision callbacks
+      table.insert(assteroids, assteroid) 
     end
-
-end
-
-function love.keypressed(key)
-   if key == 'escape' then
-      love.event.push('quit')
-   end
 end
 
 function wrapScreen()
